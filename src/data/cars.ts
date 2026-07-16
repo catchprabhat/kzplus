5222ecb3a3e94a6e3b0870ab5362cbf8e8319b47
prpadhy <prpadhy@cisco.com>
2026-07-16 18:58:22 +0530

    new cars added
    new cars added

--- src/data/cars.ts
+++ src/data/cars.ts
@@ -5,9 +5,11 @@ import dusterKA from '../images/duster-KA.jpg';
 import duster1 from '../images/duster1.jpg';
 import baleno from '../images/balenoo.jpg';
 import Poloo from '../images/polo.jpg';
+import PoloNew from '../images/poloNew.png';
 import ciaz from '../images/ciaz.jpg';
-import innovaWhite from '../images/innova-white.jpg';
-import Crysta from '../images/Innova.jpg';
+import Crysta from '../images/Innova.png';
+import swift from '../images/swiftM.png';
+import ertiga from '../images/ertigaNew.png';
 
 export const cars: Car[] = [
   {
@@ -19,7 +21,7 @@ export const cars: Car[] = [
     transmission: 'Automatic',
     fuel: 'Diesel',
     pricePerDay: 7000,
-    pricePerHour: 280,
+    pricePerHour: 400,
     features: ['AWD', 'Panoramic Roof', 'Premium Sound', '18 Inch Tyres'],
     available: true,
     subscription: 3400
@@ -33,24 +35,24 @@ export const cars: Car[] = [
     transmission: 'Automatic',
     fuel: 'Diesel',
     pricePerDay: 6000,
-    pricePerHour: 250,
+    pricePerHour: 400,
     features: ['Captain-Seats', 'Premium Audio'],
     available: true,
     subscription: 3000
   },
   {
     id: '3',
-    name: 'Innova Crysta',
-    image: innovaWhite,
+    name: 'Ertiga',
+    image: ertiga,
     type: 'SUV',
     seats: 7,
-    transmission: 'Automatic',
-    fuel: 'Diesel',
-    pricePerDay: 6000,
-    pricePerHour: 250,
+    transmission: 'Manual',
+    fuel: 'Petrol',
+    pricePerDay: 4000,
+    pricePerHour: 350,
     features: ['Captain-Seats', 'Premium Audio'],
     available: true,
-    subscription: 3000
+    subscription: 2500
   },
   
   {
@@ -122,5 +124,33 @@ export const cars: Car[] = [
     features: ['Auto Climate Control'],
     available: true,
     subscription: 1800
+  },
+  {
+    id: '9',
+    name: 'Swift',
+    image: swift,
+    type: 'Hatchback',
+    seats: 5,
+    transmission: 'Manual',
+    fuel: 'Petrol',
+    pricePerDay: 3000,
+    pricePerHour: 150,
+    features: ['Auto Climate Control'],
+    available: true,
+    subscription: 1800
+  },
+  {
+    id: '10',
+    name: 'Baleno 2026',
+    image: PoloNew,
+    type: 'Hatchback',
+    seats: 5,
+    transmission: 'Manual',
+    fuel: 'Petrol',
+    pricePerDay: 3500,
+    pricePerHour: 200,
+    features: ['Brand-new', 'Premium Audio'],
+    available: true,
+    subscription: 1500
   }
 ];
